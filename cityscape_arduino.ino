f00991b3005d0d91f0870b2c5b0d325e9c1ea9e5
sophiagb9 <sophia_gluskin-braun@brown.edu>
2017-05-07 20:04:21 -0400

    begin bluetooth services

--- cityscape_arduino.ino
+++ cityscape_arduino.ino
@@ -1,6 +1,17 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_UART.h"
+
+#include "BluefruitConfig.h"
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(F("Cityscape Board Microcontroller"));
+  Serial.println(F("---------------------------------------------------"));
 
+  randomSeed(micros());
 }
 
 void loop() {
@@ -8,14 +19,54 @@ void loop() {
 
 }
 
+//Utility
+void error(const __FlashStringHelper*err) {
+  Serial.println(err);
+  while (1);
+}
+
 //Bluetooth Lifecycle
-#define CITYSCAPESERVICE 0xCC00
+SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
+Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
+                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
+
+#define GATTSERV_CITYSCAPE 0xCC00
+#define GATTCHAR_BOARDSSTATE 0xCC01
+#define GATTCHAR_BOARDPOWERSTATES 0xCC02
+#define GATTCHAR_BOARDBUILDINGSTATES 0xCC03
+#define GATTCHAR_BOARDTRANSITSTATES 0xCC04
 
 int32_t csServiceId;
-int32_t csBoardStateCharId;
-int32_t csBoardUpdateCharId;
+int32_t csBoardSStateCharId;
+int32_t csBoardPowerStatesCharId;
+int32_t csBoardBuildingStatesCharId;
+int32_t csBoardTransitStatesCharId;
 
 int initializeBluetoothService() {
+  boolean success;
+
+  /* Initialise the module */
+  Serial.print(F("Initialising the Bluefruit LE module: "));
+
+  if ( !ble.begin(VERBOSE_MODE) )
+  {
+    error(F("Couldn't find Bluefruit, make sure it's in CoMmanD mode & check wiring?"));
+  }
+  Serial.println( F("OK!") );
+
+  /* Perform a factory reset to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  if (! ble.factoryReset() ){
+       error(F("Couldn't factory reset"));
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+
+  Serial.println("Requesting Bluefruit info:");
+  /* Print Bluefruit information */
+  ble.info();
+  
   /* Change the device name to make it easier to find */
   Serial.println(F("Setting device name to 'Cityscape Board': "));
 
@@ -26,10 +77,19 @@ int initializeBluetoothService() {
 
   /* Add the Cityscape Service definition */
   /* Service ID should be 1 */
-  Serial.println(F("Adding the Cityscape Service definition (UUID = CITYSCAPESERVICE): "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=CITYSCAPESERVICE"), &csServiceId);
+  Serial.println(F("Adding the Cityscape Service definition (UUID = GATTSERV_CITYSCAPE): "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID=GATTSERV_CITYSCAPE"), &csServiceId);
   if (! success) {
     error(F("Could not add Cityscape service"));
+  }
+
+  /* Add the Structure State characteristic */
+  /* Chars ID for Measurement should be 1 */
+  //TODO: Min/max len
+  Serial.println(F("Adding the Structure State characteristic (UUID = GATTCHAR_BOARDSSTATE): "));
+  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID=GATTCHAR_BOARDSSTATE, PROPERTIES=0x10, MIN_LEN=1, MAX_LEN=20, VALUE=00-00, DESCRIPTION=Structure State"), &csBoardSStateCharId);
+    if (! success) {
+    error(F("Could not add Structure State characteristic"));
   }
 }
 

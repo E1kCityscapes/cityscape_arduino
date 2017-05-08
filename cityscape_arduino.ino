f22236a6dce4dd85467c114e7fb32293be0dd8f1
sophiagb9 <sophia_gluskin-braun@brown.edu>
2017-05-07 23:16:14 -0400

    add dumb spi

--- cityscape_arduino.ino
+++ cityscape_arduino.ino
@@ -1,6 +1,8 @@
 #include <Arduino.h>
+#include <SPI.h>
 #include <SoftwareSerial.h>
 #include "Adafruit_BLE.h"
+#include "Adafruit_BluefruitLE_SPI.h"
 #include "Adafruit_BluefruitLE_UART.h"
 
 #include "BluefruitConfig.h"
